27ee32d4388745fcbd8a72742fc29f1a38b7fe01
petrutivanoiu <ivanoiu.petrut27@gmail.com>
2024-10-29 13:58:00 +0200

    feat: init

---
+++ src/views/PostsView.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const posts = ref()
+
+async function fetchPosts() {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    if (!response.ok) {
+      throw new Error('Network response was not ok')
+    }
+    const data = await response.json()
+    posts.value = data
+  } catch (error) {
+    console.error('There has been a problem with your fetch operation:', error)
+  }
+}
+
+onMounted(() => {
+  fetchPosts()
+})
+</script>
+
+<template>
+  <h1>Posts</h1>
+  <main>
+    {{ posts }}
+  </main>
+</template>
